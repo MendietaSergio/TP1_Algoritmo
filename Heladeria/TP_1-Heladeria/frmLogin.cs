0631d9d9cb181db682a05b63afcb6e586e367a42
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2026-06-30 00:07:54 -0300

    Add:proyecto inicial

---
+++ Heladeria/TP_1-Heladeria/frmLogin.cs
@@ -0,0 +1,10 @@
+namespace TP_1_Heladeria
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Heladeria/TP_1-Heladeria/frmRecuperarContrasena.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP_1_Heladeria
+{
+    public partial class frmRecuperarContrasena : Form
+    {
+        public frmRecuperarContrasena()
+        {
+            InitializeComponent();
+        }
+    }
+}
