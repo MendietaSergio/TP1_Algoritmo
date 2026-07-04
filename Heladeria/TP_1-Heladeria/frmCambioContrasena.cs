1ca178c7d35153b8da0bf22bec528af2f4f1b392
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2026-07-04 14:39:13 -0300

    Agrego formulario para 'cambio contrasenia', con validaciones

--- Heladeria/TP_1-Heladeria/frmCambioContrasena.cs
+++ Heladeria/TP_1-Heladeria/frmCambioContrasena.cs
@@ -13,6 +13,92 @@ namespace TP_1_Heladeria
         public frmCambioContrasena()
         {
             InitializeComponent();
+            imgHide1.Visible = true;
+            imgShow1.Visible = false;
+            imgHide2.Visible = true;
+            imgShow2.Visible = false;
+            lblError.Visible = false;
+        }
+
+        private void imgShow1_Click(object sender, EventArgs e)
+        {
+            txtPass.PasswordChar = '*';
+            imgHide1.Visible = true;
+            imgShow1.Visible = false;
+        }
+
+        private void imgHide1_Click(object sender, EventArgs e)
+        {
+            txtPass.PasswordChar = '\0';
+            imgHide1.Visible = false;
+            imgShow1.Visible = true;
+        }
+
+        private void imgShow2_Click(object sender, EventArgs e)
+        {
+            txtPass2.PasswordChar = '*';
+            imgHide2.Visible = true;
+            imgShow2.Visible = false;
+        }
+
+        private void imgHide2_Click(object sender, EventArgs e)
+        {
+            txtPass2.PasswordChar = '\0';
+            imgHide2.Visible = false;
+            imgShow2.Visible = true;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtPass.Text = "";
+            txtPass2.Text = "";
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            lblError.Visible=false;
+            lblError.Text = "Error: ";
+            if (txtPass.Text == "")
+            {
+                lblError.Visible = true;
+                lblError.Text = lblError.Text + "Campo vacio, revisalos.";
+                txtPass.Focus();
+                return;
+            }
+            if (txtPass2.Text == "")
+            {
+                lblError.Visible = true;
+                lblError.Text = lblError.Text + "Campo vacio, revisalos.";
+                txtPass2.Focus();
+                return;
+            }
+            if(txtPass.MaxLength <= 8 )
+            {
+                lblError.Visible = true;
+                lblError.Text = lblError.Text + "Contraseña menor a 8 caracteres.";
+                txtPass.Focus();
+                return;
+            }
+            if (txtPass2.MaxLength <= 8)
+            {
+                lblError.Visible = true;
+                lblError.Text = lblError.Text + "Contraseña menor a 8 caracteres.";
+                txtPass2.Focus();
+                return;
+            }
+            
+            if (txtPass.Text != txtPass2.Text)
+            {
+                lblError.Visible = true;
+                lblError.Text = lblError.Text + "Contraseñas no coinciden.";
+                txtPass.Focus();
+
+                return;
+            }
+            MessageBox.Show(
+                "¡La contraseña se generó exitosamente!",
+                    "Cambio guardado", MessageBoxButtons.OK, MessageBoxIcon.Information
+                    );
         }
     }
 }
--- Heladeria/TP_1-Heladeria/frmLogin.cs
+++ Heladeria/TP_1-Heladeria/frmLogin.cs
@@ -6,5 +6,12 @@ namespace TP_1_Heladeria
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmCambioContrasena frmCambioContrasenia = new frmCambioContrasena();
+
+            frmCambioContrasenia.ShowDialog();
+        }
     }
 }
